bbef29d7a32d14511a34e8659be571d986ae0126
amrita2084 <amrita2084@yahoo.com>
2025-12-11 07:50:56 +0530

    Added my first Django project

---
+++ Initial_projects/mynewproject/app1/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+def welcomeview(request):
+    msg="<h2>Welcome to Django</h2"
+    out=HttpResponse(msg)
+    return out
