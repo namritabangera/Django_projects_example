80682f006d9fef27f658c4abbaa5bb5ae00b03fc
amrita2084 <amrita2084@yahoo.com>
2025-12-29 08:24:24 +0530

    Form validation example

--- Initial_projects/proj16/app1/views.py
+++ Initial_projects/proj16/app1/views.py
@@ -1,7 +1,25 @@
 from django.shortcuts import render
 from app1.forms import StudentForm
+from django.http import HttpResponse
 
 # Create your views here.
+
+flag=True
+
 def studview(request):
-    studform=StudentForm()
-    return render(request,"student.html.",context={'stud':studform})
+    global flag
+    if flag:
+       studform=StudentForm()
+       flag=False
+       return render(request,"student.html.",context={'stud':studform})
+    else:
+        flag=True
+        studform=StudentForm(request.GET)
+        if studform.is_valid():
+            rollno=studform.cleaned_data['rollno']
+            name=studform.cleaned_data['name']
+            course=studform.cleaned_data['course']
+            print(rollno,name,course)
+        studform=StudentForm()    
+        return render(request,"student.html",context={'stud':studform})
+        
